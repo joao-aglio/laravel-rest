60648411e22d8b3d8c15c019d991280c1b56b2df
joao-aglio <85963939+joao-aglio@users.noreply.github.com>
2023-09-08 14:52:07 -0300

    add append (Book model) and remove cors policy

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,12 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
     protected $guarded = ['id'];  
+
+    protected $with = ['category'];
+
+    protected $appends = ['image_url'];
+
+    public function category(){
+        return $this->hasOne(Category::class, 'id', 'category_id');
+    }
+
+    public function getImageUrlAttribute($size=null)
+    {
+        return asset('images/' . $this->cover);
+    }
     
-    use HasFactory;
 }
